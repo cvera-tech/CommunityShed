e50266e11e07ef65c1a42517ea8c895304faccac
jbvera <johnbenedictv@gmail.com>
2019-09-06 16:40:38 -0400

    Add session-backed CurrentCommunityId property to BasePage

--- CommunityShed/BasePage.cs
+++ CommunityShed/BasePage.cs
@@ -16,8 +16,35 @@ namespace CommunityShed
             FROM Person
             WHERE Username = @Username
         ";
+        private const string GetCommunitiesQuery = @"
+            SELECT C.Id
+            FROM Community C
+                JOIN CommunityPerson CP ON CP.CommunityId = C.Id
+                JOIN Person P ON CP.PersonId = P.Id
+            WHERE P.Id = @PersonId
+        ";
 
         protected int? userId;
+
+        protected int CurrentCommunityId
+        {
+            get
+            {
+                object o = Session["CommunityId"];
+                if (o != null)
+                {
+                    return (int)o;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            set
+            {
+                Session["CommunityId"] = value;
+            }
+        }
 
         protected void Page_Init(object sender, EventArgs e)
         {
@@ -25,6 +52,17 @@ namespace CommunityShed
             DataTable userTable = DatabaseHelper.Retrieve(GetUserQuery,
                 new SqlParameter("@Username", username));
             userId = userTable.Rows[0].Field<int>("Id");
+            
+            // No community in session
+            if (CurrentCommunityId < 0)
+            {
+                DataTable communitiesTable = DatabaseHelper.Retrieve(GetCommunitiesQuery,
+                    new SqlParameter("@PersonId", userId));
+                if (communitiesTable.Rows.Count > 0)
+                {
+                    CurrentCommunityId = communitiesTable.Rows[0].Field<int>("Id");
+                }
+            }
         }
     }
 }
